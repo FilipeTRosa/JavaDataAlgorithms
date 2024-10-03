5406e8978211dc3c3b0a87fb03747b93c919a7ff
Filipe Teixeira <filipetrosa@gmail.com>
2024-10-03 17:34:32 -0300

    Estrutura de classes

--- Data_Sorting/src/Main.java
+++ Data_Sorting/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] vetor = new int[10];
+
+        for (int i=0; i < vetor.length; i++){
+            vetor[i] = (int) Math.floor(Math.random() * vetor.length); //gera um int aleatorio
+        }
+
+        for(int i=0; i<vetor.length; i++) {
+                System.out.println(vetor[i]);
+            }
+        }
     }
-}
